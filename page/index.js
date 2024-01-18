0415453a6c81c35b66fc0740d24d51b3f62ea96d
Bianchi-marina <141640079+Bianchi-marina@users.noreply.github.com>
2024-01-18 00:08:07 -0300

    commit da branch testando

--- page/index.js
+++ page/index.js
@@ -4,50 +4,45 @@ import DepositModal from '../components/depositoModal.js';
 import ApostaModal from '../components/apostaModal.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const loginModal = new LoginModal();
-    const userModal = new UserModal();
-    const depositModal = new DepositModal();
-    const apostaModal = new ApostaModal();
-    
-    const profileBtn = document.getElementById('profileBtn');
-    const moneyBtn = document.getElementById('moneyBtn');
-    const apostaBtn = document.querySelector('time-btn');
-    // const apostaBtn = document.querySelectorAll('.time-btn');
+  const loginModal = new LoginModal();
+  const userModal = new UserModal();
+  const depositModal = new DepositModal();
+  const apostaModal = new ApostaModal();
+  
+  const profileBtn = document.getElementById('profileBtn');
+  const moneyBtn = document.getElementById('moneyBtn');
+  const apostaBtns = document.querySelectorAll('.time-btn');
 
-    // apostaBtn.forEach((btn) => {
-    //     btn.addEventListener('click', () => {
-    //       apostaModal.open();
-    //     });
-    // });
+  apostaBtns.forEach((btn) => {
+      btn.addEventListener('click', () => {
+          apostaModal.open();
+      });
+  });
 
-    apostaBtn.addEventListener('click', () => {
-        apostaModal.open();
-    });
+  profileBtn.addEventListener('click', () => {
+      userModal.open();
+  });
+  
+  document.querySelector('.close').addEventListener('click', () => {
+    userModal.close();
+    depositModal.close();
+    apostaModal.close();
+  });
+  
+  document.querySelector('.button__depositar').addEventListener('click', () => {
+      userModal.close();
+      depositModal.open();
+  });
+  
+  moneyBtn.addEventListener('click', () => {
+      depositModal.open();
+  }); 
 
-    profileBtn.addEventListener('click', () => {
-        userModal.open();
-    });
-    
-    document.querySelector('.close').addEventListener('click', () => {
+  window.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
       userModal.close();
       depositModal.close();
       apostaModal.close();
-    });
-    
-    document.querySelector('.button__depositar').addEventListener('click', () => {
-        userModal.close();
-        depositModal.open();
-    });
-    
-    moneyBtn.addEventListener('click', () => {
-        depositModal.open();
-    }); 
-  
-    window.addEventListener('keydown', (event) => {
-      if (event.key === 'Escape') {
-        userModal.close();
-        depositModal.close();
-        apostaModal.close();
-      }
-    });
+    }
+  });
 });
